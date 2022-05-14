3e5e5685566b0468440200e1ada4b535669bcd98
Md Naeem talukder <naeemtalukder169135@gmail.com>
2022-05-15 05:08:08 +0600

    express and mongodb server site conection

--- src/Pages/Apointment/Service.js
+++ src/Pages/Apointment/Service.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Service = ({ service, setTreatment }) => {
     const { slots, name } = service;
     return (
-        <div class="card lg:mx-w-lg bg-base-100 shadow-xl">
-            <div class="card-body">
-                <h2 class="card-title text-secondary">{name}</h2>
+        <div class="card lg:mx-w-lg bg-base-100 shadow-xl my-12">
+            <div class="card-body text-center">
+                <h2 class="text-xl font-bold text-secondary">{name}</h2>
                 <p>
                     {
                         slots.length > 0
@@ -18,7 +18,7 @@ const Service = ({ service, setTreatment }) => {
                         for="booking-modal"
                         disabled={slots.length === 0}
                         onClick={() => setTreatment(service)}
-                        class="btn btn-secondary text-white uppercase">Book Appointment
+                        class="btn btn-sm text-white uppercase bg-gradient-to-r from-secondary to-primary">Book Appointment
                     </label>
                 </div>
             </div>
