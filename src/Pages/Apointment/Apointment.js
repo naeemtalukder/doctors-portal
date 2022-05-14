99973b600a33231f0360b72b73b32a2e96625f79
Md Naeem talukder <naeemtalukder169135@gmail.com>
2022-05-14 20:27:25 +0600

    Calander add and Date Selected

---
+++ src/Pages/Apointment/Apointment.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../Shared/Footer';
+import ApointmentBanner from './ApointmentBanner';
+
+const Apointment = () => {
+    return (
+        <div>
+            <ApointmentBanner></ApointmentBanner>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Apointment;
